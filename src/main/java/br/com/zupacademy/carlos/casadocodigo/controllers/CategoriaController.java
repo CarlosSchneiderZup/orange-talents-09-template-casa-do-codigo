5a1233c3e9d46531c290bfd93f53000f6daa0a8f
CarlosG <carlos.schneider@zup.com.br>
2021-09-27 15:13:59 -0300

    implementação de um validador genérico

--- src/main/java/br/com/zupacademy/carlos/casadocodigo/controllers/CategoriaController.java
+++ src/main/java/br/com/zupacademy/carlos/casadocodigo/controllers/CategoriaController.java
@@ -3,15 +3,11 @@ package br.com.zupacademy.carlos.casadocodigo.controllers;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import br.com.zupacademy.carlos.casadocodigo.controllers.components.GaranteNomeUnicoValidator;
 import br.com.zupacademy.carlos.casadocodigo.controllers.forms.CategoriaForm;
 import br.com.zupacademy.carlos.casadocodigo.entities.Categoria;
 import br.com.zupacademy.carlos.casadocodigo.repositories.CategoriaRepositorio;
@@ -22,12 +18,6 @@ public class CategoriaController {
 
 	@Autowired
 	private CategoriaRepositorio categoriaRepositorio;
-	@Autowired private GaranteNomeUnicoValidator garanteNomeUnicoValidator;
-	
-	@InitBinder
-	public void configuracoesIniciais(WebDataBinder binder) {
-		binder.addValidators(garanteNomeUnicoValidator);
-	}
 	
 	@PostMapping
 	public void novaCategoria(@RequestBody @Valid CategoriaForm categoria) {
--- src/main/java/br/com/zupacademy/carlos/casadocodigo/controllers/forms/CategoriaForm.java
+++ src/main/java/br/com/zupacademy/carlos/casadocodigo/controllers/forms/CategoriaForm.java
@@ -2,11 +2,13 @@ package br.com.zupacademy.carlos.casadocodigo.controllers.forms;
 
 import javax.validation.constraints.NotBlank;
 
+import br.com.zupacademy.carlos.casadocodigo.customizations.UniqueValue;
 import br.com.zupacademy.carlos.casadocodigo.entities.Categoria;
 
 public class CategoriaForm {
 
 	@NotBlank
+	@UniqueValue(domainClass = Categoria.class, fieldName = "nome")
 	private String nome;
 
 	public String getNome() {
